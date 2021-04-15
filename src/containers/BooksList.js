1e06ded9ef829732e6d6ff21fba42d93f0d3bc78
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-16 00:22:30 +0200

    Connect the REMOVE_BOOK action to the component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,29 +1,38 @@
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
+import { REMOVE_BOOK } from '../actions';
 
-const BooksList = ({ books }) => (
+const BooksList = ({ books }) => {
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = (id) => {
+    dispatch(REMOVE_BOOK(id));
+  };
+
+  return (
   <table>
-  <thead>
-    <tr>
-      <th>Book ID</th>
-      <th>Title</th>
-      <th>Category</th>
-    </tr>
-  </thead>
-    <tbody>
-      {
-        books.map((book) => (
-          <tr key={book.id}>
-            <Book
-              book={book}
-            />
-          </tr>
-        ))
-      }
-    </tbody>
-  </table>
-);
+    <thead>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+      <tbody>
+        {
+          books.map((book) => (
+            <tr key={book.id}>
+              <Book
+                book={book} removeBookHandler={ (book) => handleRemoveBook(book)}
+              />
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  );
+};
 
 const mapStateToProps = (state) => ({
   books: state.bookReducer,
