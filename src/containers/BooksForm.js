b76673e5d7533bc2a9e9ecf603ded991630a9d81
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-15 23:28:10 +0200

    Implement handleSubmit which will save the new book in the Redux store and resets the component’s state

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { CREATE_BOOK } from '../actions';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
 const BooksForm = () => {
+  const dispatch = useDispatch();
+
   const [bookName, setBookName] = useState('');
   const [bookCat, setBookCat] = useState('');
 
   const handleInput = ({ target: { value } }) => setBookName(value);
   const handleSelect = ({ target: { value } }) => setBookCat(value);
+
+  const handleSubmit = () => {
+    dispatch(CREATE_BOOK({
+      id: Math.floor(Math.random() * 1000),
+      title: bookName,
+      category: bookCat,
+    }));
+    setBookName('');
+    setBookCat('');
+  };
 
   return (
     <>
@@ -17,7 +32,7 @@ const BooksForm = () => {
             <option key={category} value={category}>{ category }</option>
           ))}
         </select>
-        <input type="submit" />
+        <input type="submit" onClick={handleSubmit.bind(this)} />
       </form>
     </>
   );
