95e296b8908c726aceab0b4694994baa9360b49a
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-15 20:27:09 +0200

    Implement BookList component

---
+++ src/containers/BooksList.js
@@ -0,0 +1,14 @@
+const BooksList = () => (
+  <table>
+  <thead>
+    <tr>
+      <th>Book ID</th>
+      <th>Title</th>
+      <th>Category</th>
+    </tr>
+  </thead>
+    <tbody></tbody>
+  </table>
+);
+
+export default BooksList;
