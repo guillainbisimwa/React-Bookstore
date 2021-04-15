19e157cc9c0bfe6f835369ed8522b0523495c295
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-15 21:52:55 +0200

    Implement BooksForm components a very simple form for adding books

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,5 +1,19 @@
+import React from 'react';
+
+const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
 const BooksForm = () => (
-  <div>BooksForm</div>
+    <>
+      <form>
+        <input type="text" />
+        <select>
+          {categories.map((category) => (
+            <option key={category} value={category}>{ category }</option>
+          ))}
+        </select>
+        <input type="submit" />
+      </form>
+    </>
 );
 
 export default BooksForm;
