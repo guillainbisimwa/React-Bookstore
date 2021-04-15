4faa58a4799ddba64e7d6fab4e79a2541be10444
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-15 23:04:55 +0200

    Implement the handleChange method which reacts to any change in the category selection

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const BooksForm = () => {
+  const [bookValue, setBookValue] = useState('');
 
-const BooksForm = () => (
+  const handleChange = ({ target: { value } }) => setBookValue(value);
+
+  return (
     <>
       <form>
         <input type="text" />
-        <select>
+        <select onChange={handleChange.bind(this)} value={bookValue}>
           {categories.map((category) => (
             <option key={category} value={category}>{ category }</option>
           ))}
@@ -14,6 +18,7 @@ const BooksForm = () => (
         <input type="submit" />
       </form>
     </>
-);
+  );
+};
 
 export default BooksForm;
