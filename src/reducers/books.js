83aa168eb63da572dce0b824982208551b6a7afe
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-15 22:09:08 +0200

    Define the Redux actions and reducers

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,5 +1,11 @@
-const bookReducer = (state = [], action) => {
-  switch (action.type) {
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
+
+const bookReducer = (state = [], { type, payload }) => {
+  switch (type) {
+    case CREATE_BOOK:
+      return [...state, payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== payload.id);
     default:
       return state;
   }
