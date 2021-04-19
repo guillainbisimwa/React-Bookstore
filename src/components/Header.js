963a8ed5a28ab0d08a18e01e11e8b77ef9731bb9
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-19 22:42:20 +0200

    Add right icon in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { Box, IconButton } from '@material-ui/core';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,7 +18,14 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 'bolder',
     color: '#0091ff',
     fontSize: '30px',
-
+  },
+  userIcon: {
+    fontWeight: 'bolder',
+    color: '#0091ff',
+    fontSize: '30px',
+  },
+  linkContainer: {
+    flexGrow: 1,
   },
 }));
 
@@ -27,12 +35,15 @@ const Header = () => {
   return (
     <div className={classes.root}>
         <AppBar position="static" color="inherit">
-          <Toolbar justifyContent="flex-start" >
-            <Typography variant="h5" className={classes.title}>
-            Bookstore CMS
-            </Typography>
-            <p className={classes.menuButton} >BOOKS</p>
-            <p className={classes.menuButton} >CATEGORIES</p>
+          <Toolbar>
+            <Box display="flex" flexDirection="row" alignItems="center" className={classes.linkContainer}>
+              <Box variant="h5" className={classes.title}>Bookstore CM </Box>
+              <Box className={classes.menuButton} >BOOKS</Box>
+              <Box className={classes.menuButton} >CATEGORIES</Box>
+            </Box>
+            <IconButton edge="start" className={classes.userIcon} color="inherit" aria-label="menu">
+                <AccountCircleRoundedIcon />
+            </IconButton>
           </Toolbar>
         </AppBar>
     </div>
